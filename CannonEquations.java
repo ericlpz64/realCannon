3c0458129bcd6f7620832f0c414dbb458f04c6c0
ericlpz64 <72568722+ericlpz64@users.noreply.github.com>
2023-05-02 18:45:35 +0000

    real

--- CannonEquations.java
+++ CannonEquations.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 import java.lang.Math;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 
 public class CannonEquations {
     Double vi;
@@ -10,17 +14,26 @@ public class CannonEquations {
     Double theta;
     Double vix;
     Double vfx;
-
+    Double viy;
+    Double vfy;
+    Double viS;
+    Double vfS;
+    Double aS;
+    Double mS;
+    Double deltaxS;
+    Double timeS;
+    Double thetaS;
+    
     // if a variable is missing input null
     public CannonEquations(
 
-            Double velocityinitial,
-            Double velocityfinal,
-            Double acceleration,
-            Double mass,
-            Double deltax,
-            Double deltaTime,
-            Double thetaAngle) {
+        Double velocityinitial,
+        Double velocityfinal,
+        Double acceleration,
+        Double mass,
+        Double deltax,
+        Double deltaTime,
+        Double thetaAngle) {
 
         vi = velocityinitial;
         vf = velocityfinal;
@@ -32,36 +45,60 @@ public class CannonEquations {
     }
 
     public Double missingvi() {
-        vi = vf + ((a * time) * -1);
-        vix = vi * (Math.toDegrees(Math.cos(theta)));
+        if (a == 0.0) {
+            vi = vf;
+        } else {
+            vi = vf + ((a * time) * -1);
+        }
         return vi;
     }
 
     public Double missingvf() {
-        vf = vi + (a * time);
-        vfx = vf * (Math.toDegrees(Math.cos(theta)));
+        if (a == 0.0) {
+            vf = vi;
+        } else {
+            vf = vi + (a * time);
+        }
         return vf;
     }
 
     public Double missinga() {
-        a = (vf - vi) / time;
+        if (vi == vf) {
+            a = 0.0;
+        } else {
+            a = (vf - vi) / time;
+        }
         return a;
     }
 
     public Double missingawt() {
-        a = ((vf * vf) - (vi - vi)) / (2 * x);
+        if (vi == vf) {
+            a = 0.0;
+        } else {
+            a = ((vf * vf) - (vi * vi)) / (2.0 * x);
+        }
         return a;
     }
 
-    public Double missingx() {
+    public void missingvy() {
         Double thetar = Math.toRadians(theta);
-        Double thetax = Math.cos(thetar);
-        vix = thetax * vi;
-        vfx = thetax * vf;
+        Double thetas = Math.sin(thetar);
+        viy = thetas * vi;
+        vfy = thetas * vf;
+    }
+
+    public void missingvx() {
+        Double thetar = Math.toRadians(theta);
+        Double thetac = Math.cos(thetar);
+        vix = thetac * vi;
+        vfx = thetac * vf;
+    }
+
+    public Double missingx() {
         if (a != 0.0) {
-            x = (((vfx * vfx) - (vix * vix))) / (2 * a);
+            x = (((vfx * vfx) - (vix * vix))) / (2.0 * a);
         } else {
-            x = ((vfx + vix) / 2) * time;
+            x = ((vfx + vix) / 2.0) * time;
         }
         return x;
     }
@@ -75,4 +112,37 @@ public class CannonEquations {
         theta = 0.5 * (Math.toDegrees(Math.asin((9.81 * x) / (vi * vi))));
         return theta;
     }
+
+    public String toString(Double varReal){
+        if (varReal == null){return "unknown";}
+        if(varReal == vi){return varReal + "㎧";}
+        if(varReal == vf){return varReal + "㎧";}
+        if(varReal == a){return varReal + "㎧²";}
+        if(varReal == m){return varReal + "kg";}
+        if(varReal == x){return varReal + "m";}
+        if(varReal == time){return varReal + "s";}
+        if(varReal == theta){return varReal + "°";}
+        else return "woops";
+    }
+    public static void writeFile(String real) {
+        try {
+            File data = new File("result.txt");
+            Scanner reader = new Scanner(data);
+            String str = "";
+            while (reader.hasNextLine())
+                str += reader.nextLine() + "\n";
+            FileWriter f = new FileWriter("result.txt");
+            f.write(str);
+            f.write("\n" + "New Run:");
+            f.write(real);
+            f.close();
+        } catch (IOException e) {
+            System.out.println("failed to write to result.txt");
+        }
+    }
+    /*
+     * public Double airDensity() {
+     * 
+     * }
+     */
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 class Main {
     public static Double isNull(String value) {
-        if (value.equals("null")) return null;
+        if (value.equals("null"))
+            return null;
 
         try {
             return Double.parseDouble(value);
-        } catch (Exception e) { e.printStackTrace(); }
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
+
     public static void main(String[] args) {
         Double viInput;
         Double vfInput;
@@ -18,46 +23,74 @@ class Main {
         Double deltaxInput;
         Double timeInput;
         Double thetaInput;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input Initial Velocity: ");
-        viInput = isNull(sc.nextLine());
-        System.out.println("Input Final Velocity: ");
-        vfInput = isNull(sc.nextLine());
-        System.out.println("Input Acceleration: ");
-        aInput = isNull(sc.nextLine());
-        System.out.println("Input Mass: ");
-        mInput = isNull(sc.nextLine());
-        System.out.println("Input Δx: ");
-        deltaxInput = isNull(sc.nextLine());
-        System.out.println("Input Time: ");
-        timeInput = isNull(sc.nextLine());
-        System.out.println("Input θ: ");
-        thetaInput = isNull(sc.nextLine());
-        
-        CannonEquations Teracotta = new CannonEquations(viInput, vfInput, aInput, mInput, deltaxInput, timeInput, thetaInput);
-        //Teracotta.CannonEquations(50.0, 45.0, null, null, 37.2, 1.4, 15.0);//vi,vf,a,m,delatx,time,theta
-        { 
-            if(Teracotta.vi == null){
-            System.out.println("found missing initial velocity: " + Teracotta.missingvi());
-            }
-            if(Teracotta.vf == null){
-            System.out.println("found missing final velocity: " + Teracotta.missingvf());
-            }
-            if(Teracotta.a == null){
-            System.out.println("found missing acceleration: " + Teracotta.missinga());
-            }
-            if(Teracotta.m == null){
-            System.out.println("please measure the mass of this item");
-            }
-            if(Teracotta.x == null){
-            System.out.println("found missing Δx: " + Teracotta.missingx());
-            }
-            if(Teracotta.time == null){
-            System.out.println("found missing time: " + Teracotta.missingtime());
-            }
-            if(Teracotta.theta == null){
-            System.out.println("found missing θ: " + Teracotta.missingtheta());
-            }
+        System.out.print("Input Initial Velocity: ");
+        viInput = isNull(sc.next());
+        System.out.print("Input Final Velocity: ");
+        vfInput = isNull(sc.next());
+        System.out.print("Input Acceleration: ");
+        aInput = isNull(sc.next());
+        System.out.print("Input Mass: ");
+        mInput = isNull(sc.next());
+        System.out.print("Input Δx: ");
+        deltaxInput = isNull(sc.next());
+        System.out.print("Input Time: ");
+        timeInput = isNull(sc.next());
+        System.out.print("Input θ: ");
+        thetaInput = isNull(sc.next());
+
+        CannonEquations Teracotta = new CannonEquations(viInput, vfInput, aInput, mInput, deltaxInput, timeInput,
+                thetaInput);
+        // Teracotta.CannonEquations(50.0, 45.0, null, null, 37.2, 1.4,
+        // 15.0);//vi,vf,a,m,delatx,time,theta
+
+        if (Teracotta.m == null) {
+            System.out.println("please measure the mass of this item in kg");
         }
+        if (Teracotta.vi == null) {
+            System.out.println("found missing initial velocity: " + Teracotta.toString(Teracotta.missingvi()));
+        }
+        if (Teracotta.vf == null) {
+            System.out.println("found missing final velocity: " + Teracotta.toString(Teracotta.missingvf()));
+        }
+        if (Teracotta.theta == null && 
+            Teracotta.x != null) {
+            System.out.println("found missing θ: " + Teracotta.toString(Teracotta.missingtheta()));
+        Teracotta.missingvx();
+        Teracotta.missingvy();}
+        if (Teracotta.theta != null){
+        Teracotta.missingvx();
+        Teracotta.missingvy();
+        }
+
+        if (Teracotta.a == null &&
+            Teracotta.time != null) {
+            System.out.println("found missing acceleration: " + Teracotta.toString(Teracotta.missinga()));
+        }
+        if (Teracotta.a == null &&
+            Teracotta.time == null) {
+            System.out.println("found missing acceleration: " + Teracotta.toString(Teracotta.missingawt()));
+        }
+        if (Teracotta.time == null) {
+            System.out.println("found missing time: " + Teracotta.toString(Teracotta.missingtime()));
+        }
+        if (Teracotta.x == null) {
+            System.out.println("found missing Δx: " + Teracotta.toString(Teracotta.missingx()));
+        }
+        if (Teracotta.theta == null) {
+            System.out.println("found missing θ: " + Teracotta.toString(Teracotta.missingtheta()));
+        }
+        
+        Teracotta.writeFile(
+    "\n" + "Initial Velocity: " + Teracotta.toString(Teracotta.vi) + 
+    "\n" + "Final Velocity: " + Teracotta.toString(Teracotta.vf) +
+    "\n" + "Acceleration: " + Teracotta.toString(Teracotta.a) +
+    "\n" + "Mass: " + Teracotta.toString(Teracotta.m) +
+    "\n" + "Δx: " + Teracotta.toString(Teracotta.x) +
+    "\n" + "Time: " + Teracotta.toString(Teracotta.time) +
+    "\n" + "θ: " + Teracotta.toString(Teracotta.theta));
+
     }
+
 }
